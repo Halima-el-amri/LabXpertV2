e541e35d064a1e9d93a94f1f602fc1793fd6807e
EL AMRI HALIMA <elamrihalima1@gmail.com>
2024-01-16 16:46:21 +0100

    Ãintitiating

---
+++ src/main/java/org/apache/maven/archetypes/labxpertproject/service/implimentation/EchantillonServiceImpl.java
@@ -0,0 +1,6 @@
+package org.apache.maven.archetypes.labxpertproject.service.implimentation;
+
+import org.apache.maven.archetypes.labxpertproject.service.interfaces.IEchantillonService;
+
+public class EchantillonServiceImpl implements IEchantillonService {
+}
---
+++ src/main/java/org/apache/maven/archetypes/labxpertproject/service/interfaces/IEchantillonService.java
@@ -0,0 +1,4 @@
+package org.apache.maven.archetypes.labxpertproject.service.interfaces;
+
+public interface IEchantillonService {
+}
